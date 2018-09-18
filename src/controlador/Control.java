9503fe62167b0b581d68743f10e34680907778a8
underlion10 <lioneldelgado73@gmail.com>
2018-09-18 11:19:23 +0100

    1er commit Hola Mundo

---
+++ src/controlador/Control.java
@@ -0,0 +1,51 @@
+package controlador;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Control
+ */
+@WebServlet("/Control")
+public class Control extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Control() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Son las " + new Date());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		char[] nombre = request.getParameter("name").toCharArray();
+		char[] wovels = {'a', 'e', 'i', 'o', 'u'};
+		int i = 0;
+		for (char letra : nombre){
+			for(char wovel : wovels){
+				if(letra == wovel){
+					i++;
+				}
+			}
+		}
+		response.getWriter().append("Tu nombre contiene " + i + " vocales");
+	}
+
+}
